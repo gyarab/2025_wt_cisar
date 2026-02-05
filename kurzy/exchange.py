d2a0d9ae6da02962fb2a98d84c7058e0619f5a40
CisarVaclav <vaclav.cisar.s@gyarab.czgit config --global user.name CisarVaclavgit config --global user.email vaclav.cisar.s@gyarab.cz>
2026-02-05 18:35:20 +0100

    uloz prevodnik

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,7 +1,50 @@
 import httpx
 
-r = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt?date=22.01.2026')
-print(r.text)
-print(r.status)
-lines = r.text.split('\n')
-print(lines[0])
+res = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt')
+
+print("server odpovedel:", res.status_code)
+lines = res.text.split('\n')
+print("Kurzy pro den:", lines[0].split(" ")[0])
+
+line_euro = ""
+for line in lines:
+    if "EUR" in line:
+        line_euro = line
+        break
+
+if line_euro == "":
+    print("Kurz EUR nebyl nalezen")
+    exit(1)
+
+rate_str = line_euro.split('|')[-1].replace(',' , '.')
+rate = float(rate_str)
+
+print("Kurz eura je", rate, "CZK")
+
+while True:
+    print("\nZvol typ převodu:")
+    print("1 = EUR -> CZK")
+    print("2 = CZK -> EUR")
+
+    mode = input("Vyber si 1 nebo 2: ")
+
+    if mode in ("1", "2"):
+        break
+    print("Neplatná volba!")
+
+while True:
+    try:
+        amount = float(input("Zadej částku: ").replace(",", "."))
+        if amount < 0:
+            print("Částka musí byt kladná.")
+            continue
+        break
+    except ValueError:
+        print("Neplatné číslo!")
+
+if mode == "1":
+    result = amount * rate
+    print(f"{amount:.2f} EUR = {result:.2f} CZK")
+else:
+    result = amount / rate
+    print(f"{amount:.2f} CZK = {result:.2f} EUR")
